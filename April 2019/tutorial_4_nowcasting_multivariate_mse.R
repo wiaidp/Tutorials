9d6c0068fac7f0f85105cd2c515919c04156121a
wiaidp <marcadolfrudolf.wildi@gmail.com>
2019-04-11 16:41:31 +0200

    bivariate mse update

--- April 2019/tutorial_4_nowcasting_multivariate_mse.R
+++ April 2019/tutorial_4_nowcasting_multivariate_mse.R
@@ -41,14 +41,15 @@ source("Common functions/mdfa_trade_func.r")
 #   Bivariate
 
 
-
-lenh<-500
-len<-121
+# Total sample size (long in order to obtain reliable MSE-performances)
+lenh<-10000
+# In-sample span: much smaller than lenh i.e. sample performances will be 'outof-sample'
+len<-300
 # Specify AR-process
 a1<-0.9
 a1<-0.1
 # Generate series for each AR(1)-process
-set.seed(10)
+set.seed(1)
 x_long<-arima.sim(list(ar=a1),n=lenh)
 x_short<-x_long[lenh/2+(-len/2):((len/2)-1)]
 periodicity<-6
@@ -58,9 +59,11 @@ cutoff<-pi/periodicity
 M<-100
 y<-ideal_filter_func(periodicity,M,x_long)$y
 
-set.seed(12)
+set.seed(2)
 # Scaling of the idiosyncratic noise
-scale_idiosyncratic<-0.
+scale_idiosyncratic<-0.4
+if (abs(scale_idiosyncratic)==0)
+  print("Design is not uniquely specified since both explanatory series are identical (up to a shift)")
 eps<-rnorm(lenh)
 indicator<-x_long+scale_idiosyncratic*eps
 # Data: first column=target, second column=x, 
@@ -72,59 +75,53 @@ data_matrix_120<-data_matrix[lenh/2+(-len/2):((len/2)-1),]
 head(round(data_matrix_120,4))
 # Spectrum
 # One can use either the function per
-weight_func<-cbind(per(data_matrix_120[,1],T)$DFT,per(data_matrix_120[,2],T)$DFT,per(data_matrix_120[,3],T)$DFT)
+weight_func_bivariate<-cbind(per(data_matrix_120[,1],T)$DFT,per(data_matrix_120[,2],T)$DFT,per(data_matrix_120[,3],T)$DFT)
 # Or one can use spec_comp (the latter is slightly more general than per)
 #   Here we can supply the full data matrix data_matrix_120 at once: spec_comp then returns the multivariate dft 
-weight_func<-spec_comp(nrow(data_matrix_120), data_matrix_120, 0)$weight_func
+weight_func_bivariate<-spec_comp(nrow(data_matrix_120), data_matrix_120, 0)$weight_func
 
-K<-nrow(weight_func)-1
+K<-nrow(weight_func_bivariate)-1
 
 Gamma<-(0:(K))<=K*cutoff/pi
 # Nowcast (Lag=0), Backcast (Lag>0) and Forecast (Lag<0)
 Lag<-0
 # Filter length: L/K should be 'small'
-L<-2*periodicity
+L<-4*periodicity
 
 
 #weight_func<-spec_comp(120,data_matrix_120,0)$weight_func
 
 # Estimate filter coefficients
-mdfa_obj<-MDFA_mse(L,weight_func,Lag,Gamma)$mdfa_obj 
+mdfa_obj_bivariate<-MDFA_mse(L,weight_func_bivariate,Lag,Gamma)$mdfa_obj 
 # Filter coefficients
-b_mat<-mdfa_obj$b
-dimnames(b_mat)[[2]]<-c("x","leading indicator")
-dimnames(b_mat)[[1]]<-paste("Lag ",0:(L-1),sep="")#dim(b_mat)
-head(b_mat)
+b__bivariate<-mdfa_obj_bivariate$b
+dimnames(b__bivariate)[[2]]<-c("x","leading indicator")
+dimnames(b__bivariate)[[1]]<-paste("Lag ",0:(L-1),sep="")#dim(b_mat)
+head(b__bivariate)
 
 
 
 
 
-yhat_bivariate_leading_indicator<-filt_func(data_matrix[,2:ncol(data_matrix)],b_mat)$yhat
+yhat_bivariate_leading_indicator<-filt_func(data_matrix[,2:ncol(data_matrix)],b__bivariate)$yhat
 
 
-weight_func_univariate<-weight_func[,1:2]
-mdfa_obj<-MDFA_mse(L,weight_func_univariate,Lag,Gamma)$mdfa_obj 
+weight_func_univariate<-weight_func_bivariate[,1:2]
+mdfa_obj_univariate<-MDFA_mse(L,weight_func_univariate,Lag,Gamma)$mdfa_obj 
 # Filter coefficients
-b<-mdfa_obj$b
+b_univariate<-mdfa_obj_univariate$b
 
-yhat_univariate<-filt_func(as.matrix(data_matrix[,2]),b)$yhat
+yhat_univariate<-filt_func(as.matrix(data_matrix[,2]),b_univariate)$yhat
 
-
-length(yhat_bivariate_leading_indicator)
-length(y)
 
 
 y_target_leading_indicator<-y
-perf_mse<-matrix(c(mean(na.exclude((yhat_bivariate_leading_indicator-
-                                      y_target_leading_indicator))^2),
-                   mean(na.exclude((yhat_univariate-
-                                      y_target_leading_indicator))^2)),nrow=1)
-dimnames(perf_mse)[[2]]<-c("bivariate MDFA","DFA")
-dimnames(perf_mse)[[1]]<-"Sample MSE"
+perf_mse<-rbind(c(mean(na.exclude((yhat_bivariate_leading_indicator-y_target_leading_indicator))^2),mean(na.exclude((yhat_univariate-y_target_leading_indicator))^2)),
+                c(round(mdfa_obj_bivariate$MS_error,3),round(mdfa_obj_univariate$MS_error,3)))
+colnames(perf_mse)<-c("bivariate MDFA","DFA")
+rownames(perf_mse)<-c("Sample MSE out-of-sample (time domain)","Criterion values in-sample (frequency-domain)")
 round(perf_mse,3)
 # Compare MSE of bivariate (time-domain) with criterion value (frequency-domain)
-round(mdfa_obj$MS_error,3)
 
 
 par(mfrow=c(1,1))
@@ -166,6 +163,12 @@ mtext("MDFA", side = 3, line = -3,at=len/2,col="green")
 
 
 
+# Discussion of results
+
+lenh<-10000
+# In-sample span: much smaller than lenh i.e. sample performances will be 'outof-sample'
+len<-120
+a1<-0.1
 
 
 
